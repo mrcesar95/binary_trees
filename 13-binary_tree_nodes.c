9bbecfd01a82dffc7c5a8fd0ccee4b7c447553ed
Cesar Molina <cesaruxfxtx@gmail.com>
2022-03-23 10:44:37 -0500

    Adding 12-binary_tree_leaves.c and 13-binary_tree_nodes.c files

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the nodes with at least one Child
+ * @tree: Pointer to tree's root
+ *
+ * Return: Number of nodes that have at least one child
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		count = 1;
+
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+
+	return (count);
+}
