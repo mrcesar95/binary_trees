4079c4a1afebc4aee1e6b1dc8be15dd71f76eb46
Cesar Molina <cesaruxfxtx@gmail.com>
2022-03-24 11:25:08 -0500

    Adding 130-binary_tree_is_heap.c file

--- binary_trees.h
+++ binary_trees.h
@@ -24,6 +24,7 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 
 
@@ -54,6 +55,7 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 int binary_tree_is_avl(const binary_tree_t *tree);
+int binary_tree_is_heap(const binary_tree_t *tree);
 
 
 #endif
